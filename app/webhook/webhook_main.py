0e4f9b9cfe6e4de44942bcc0562897bde7535f92
akshaybhalotia <github@axy.one>
2022-07-21 18:29:36 +0530

    refactor: configure webhook on app startup event

--- app/webhook/webhook_main.py
+++ app/webhook/webhook_main.py
@@ -13,6 +13,10 @@ async def receive_webhook(request: Request):
     return await analyse_and_store_webhook(request=request)
 
 
-if __name__ == "__main__":
+@app.on_event("startup")
+async def startup_event():
     configure_webhook()
+
+
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9003, forwarded_allow_ips="*", )
